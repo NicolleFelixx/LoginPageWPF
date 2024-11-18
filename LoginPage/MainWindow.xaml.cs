afb6c227a7da66ed198c8031ef8d98331e81dbc4
nicolle felix <nicolle.felix2002@gmail.com>
2024-11-18 09:36:48 -0300

    atualização

--- LoginPage/MainWindow.xaml.cs
+++ LoginPage/MainWindow.xaml.cs
@@ -1,13 +1,7 @@
-﻿using System.Text;
+﻿using System;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace LoginPage
 {
@@ -19,6 +13,30 @@ namespace LoginPage
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
+
+        private bool IsMaximize = false;
+        private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount == 2)
+            {
+                if (IsMaximize)
+                {
+                    this.WindowState = WindowState.Normal;
+                    this.Width = 1024;
+                    this.Height = 720;
+
+                    IsMaximize = false;
+                }
+            }
         }
     }
 }
